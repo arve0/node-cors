ed8e19a2b000b2c4fdd16481fbc9750938d8bdac
Arve Seljebu <arve.seljebu@gmail.com>
2020-05-07 19:56:18 +0200

    simple debug-proxy which outputs body, headers and status code

--- index.js
+++ index.js
@@ -1,85 +1,96 @@
-var http = require('http');
-var https = require('https');
-var url = require('url');
+var http = require("http");
+var https = require("https");
+var URL = require("url");
 
 var validURL = /^https?:\/\/[a-z\u00a1-\uffff0-9]+/;
 
-module.exports = app
-function app (request, response) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
+module.exports = app;
+function app(request, response) {
+  response.on("error", handleError(response, request));
+  request.on("error", handleError(response, request));
 
-  response.on('error', handleError(response, request));
-  request.on('error', handleError(response, request));
+  var body = "";
+  request.on("data", (chunk) => (body += chunk));
+  request.on("end", proxyRequest);
 
-  if (request.method !== 'GET') {
-    // fail
-    response.statusCode = 500;
-    response.write('Server only support GET requests.');
-    response.end();
-    return;
-  }
-
-  var requestedURL = request.url.slice(1);
-  if (requestedURL === '') {
-    // INDEX
-    response.write('Usage: http://' + request.headers.host + '/URL');
-    response.end();
-    return;
-  } else if (requestedURL.search(validURL) !== 0) {
-    // INVALID URL
-    response.statusCode = 500;
-    response.write('URL must be valid, got: ' + requestedURL);
-    response.end();
-    return;
-  } else {
-    // PROXY REQUEST
-    var options = url.parse(requestedURL);
-    options.headers = stripHeaders(request.headers);
-
-    if (requestedURL.slice(0, 5) === 'https') {
-      https.get(options, handleGet(response, request))
-          .on('error', handleError(response, request));
+  function proxyRequest() {
+    var requestedURL = request.url.slice(1);
+    if (requestedURL === "") {
+      // INDEX
+      response.write("Usage: http://" + request.headers.host + "/URL");
+      response.end();
+      return;
+    } else if (requestedURL.search(validURL) !== 0) {
+      // INVALID URL
+      response.statusCode = 500;
+      response.write("URL must be valid, got: " + requestedURL);
+      response.end();
+      return;
     } else {
-      http.get(options, handleGet(response, request))
-          .on('error', handleError(response, request));
+      // PROXY REQUEST
+      var url = URL.parse(requestedURL);
+      var options = {
+        headers: request.headers,
+      };
+      options.headers.host = url.host;
+      delete options.headers.connection;
+      options.method = request.method;
+
+      console.log(options.method, requestedURL);
+      console.log("REQUEST HEADERS\n", options.headers);
+      console.log("REQUEST BODY\n", body);
+
+      var responseBody = "";
+      if (requestedURL.slice(0, 5) === "https") {
+        https
+          .request(requestedURL, options, handleResponse(response))
+          .on("error", handleError(response))
+          .end(body);
+      } else {
+        http
+          .request(requestedURL, options, handleResponse(response))
+          .on("error", handleError(response))
+          .end(body);
+      }
     }
   }
 }
 
-function handleError (response, request) {
+function handleError(response) {
   return function (err) {
+    console.error(err.message);
     console.error(err.stack);
+
     // send error
     response.statusCode = 500;
-    response.write('Error.');
+    response.write("Error.");
     response.end();
-  }
+  };
 }
 
-function handleGet (response, request) {
+function handleResponse(response) {
   return function (res) {
     response.statusCode = res.statusCode;
 
-    for (var header in res.headers) {
-      if (header === 'access-control-allow-origin' || !res.headers.hasOwnProperty(header)) {
-        continue;
+    var body = "";
+
+    res.on("data", (chunk) => {
+      body += chunk;
+      response.write(chunk);
+    });
+    res.on("close", done);
+
+    function done(err) {
+      if (err) {
+        handleError(response)(err);
+        return;
       }
-      response.setHeader(header, res.headers[header]);
-    }
 
-    res.on('data', response.write.bind(response));
-    res.on('end', response.end.bind(response));
-  }
-}
+      console.log("RESPONSE STATUS\n", res.statusCode);
+      console.log("RESPONSE HEADERS\n", res.headers);
+      console.log("RESPONSE BODY\n", body);
 
-function stripHeaders (headers) {
-  var strippedHeaders = {};
-  for (var header in headers) {
-    if (header !== 'host' &&
-        header !== 'origin' &&
-        headers.hasOwnProperty(header)) {
-      strippedHeaders[header] = headers[header];
+      response.end(err);
     }
-  }
-  return strippedHeaders;
+  };
 }
